235e8a245167d0425439c3846668d4d5cd7432ca
JuanJoII <juanjose34251@gmail.com>
2026-04-19 16:56:54 -0500

    Initial Commit: Entorno de desarrollo Sveltkit + NetCore 9 + SQL Server gestionado con docker

---
+++ rp-front/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<h2>Probando hot reload</h2>
+<h3>otra prueba jsjsjs</h3>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
